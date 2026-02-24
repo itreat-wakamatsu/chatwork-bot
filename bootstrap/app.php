e4af405fedb9f423bc03f2d5eb4175d91ea78078
itreat-wakamatsu <k.wakamatsu@itreat.co.jp>
2026-02-24 15:42:04 +0900

    fix: webhook url をcsrf チェックから除外

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'webhook/chatwork',
+        ]);
         $middleware->alias([
             'chatwork.signature' => VerifyChatworkSignature::class,
         ]);
